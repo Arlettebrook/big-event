179f1e869f828a91b390bfbbfd176bcb9ea8202f
Arlettebrook <arlettebrook@foxmail.com>
2023-11-23 22:14:02 +0800

    feat: user登录接口

--- src/main/java/com/example/bigevent/controller/UserController.java
+++ src/main/java/com/example/bigevent/controller/UserController.java
@@ -3,6 +3,7 @@ package com.example.bigevent.controller;
 import com.example.bigevent.pojo.Result;
 import com.example.bigevent.pojo.User;
 import com.example.bigevent.service.UserService;
+import com.example.bigevent.utils.Md5Util;
 import jakarta.validation.constraints.Pattern;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
@@ -19,16 +20,36 @@ public class UserController {
     private UserService userService;
 
     @PostMapping("/register")
-    public Result register(@Pattern(regexp = "^\\S{5,16}$") String username,@Pattern(regexp = "^\\S{5,16}$") String password){
+    public Result register(@Pattern(regexp = "^\\S{5,16}$") String username,
+                           @Pattern(regexp = "^\\S{5,16}$") String password) {
         // 查询用户
         User u = userService.findByUserName(username);
-        if(u == null){
+        if (u == null) {
             // 没有占用注册
-            userService.register(username,password);
+            userService.register(username, password);
             return Result.success();
-        }else{
+        } else {
             //用户名被占用
             return Result.error("用户名已被占用！");
         }
+    }
+
+    @PostMapping("/login")
+    public Result login(@Pattern(regexp = "^\\S{5,16}$") String username,
+                        @Pattern(regexp = "^\\S{5,16}$") String password) {
+        //根据用户名查询用户
+        User loginUser = userService.findByUserName(username);
+        //判断用户是否存在
+        if(loginUser == null){
+            return Result.error("用户不存在！");
+        }
+
+        // 判断密码是否正确
+        if(Md5Util.getMD5String(password).equals(loginUser.getPassword())){
+            //登录成功
+            return Result.success("jwt token令牌");
+        }
+
+        return Result.error("密码错误！");
     }
 }
