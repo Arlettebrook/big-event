8a7b3fa7ebc5434f2822753f031eb9a69e6779e2
Arlettebrook <arlettebrook@foxmail.com>
2023-11-24 11:59:08 +0800

    feat: 获取用户详细信息：jsonIgnore，camel

--- src/main/java/com/example/bigevent/controller/UserController.java
+++ src/main/java/com/example/bigevent/controller/UserController.java
@@ -8,9 +8,7 @@ import com.example.bigevent.utils.Md5Util;
 import jakarta.validation.constraints.Pattern;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -50,14 +48,25 @@ public class UserController {
 
         // 判断密码是否正确
         if (Md5Util.getMD5String(password).equals(loginUser.getPassword())) {
-            //登录成功
-            Map<String,Object> claims = new HashMap<>();
-            claims.put("id",loginUser.getId());
-            claims.put("username",loginUser.getUsername());
+            //登录成功，生成token
+            Map<String, Object> claims = new HashMap<>();
+            claims.put("id", loginUser.getId());
+            claims.put("username", loginUser.getUsername());
             String token = JwtUtil.genToken(claims);
             return Result.success(token);
         }
 
         return Result.error("密码错误！");
+    }
+
+    @GetMapping("/userInfo")
+    public Result<User> userInfo(@RequestHeader("Authorization") String token) {
+//        根据token获取用户名
+        Map<String, Object> map = JwtUtil.parseToken(token);
+        String username = (String) map.get("username");
+
+//        根据用户名查询用户，获取用户信息
+        User user = userService.findByUserName(username);
+        return Result.success(user);
     }
 }
